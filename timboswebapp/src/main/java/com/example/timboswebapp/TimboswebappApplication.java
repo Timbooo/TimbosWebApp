c91875e16355ed6fc137c46ed7ec9eaadc38b365
Timbooo <96234873+Timbooo@users.noreply.github.com>
2021-12-23 11:41:46 +0100

    Anlage Projekt mit SpringBoot und Postgres

---
+++ timboswebapp/src/main/java/com/example/timboswebapp/TimboswebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.timboswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimboswebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TimboswebappApplication.class, args);
+  }
+
+}
---
+++ timboswebapp/src/test/java/com/example/timboswebapp/TimboswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timboswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimboswebappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
